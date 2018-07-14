8f1a6fc9922df4542265bfdb88a60d0f0fb095a9
Claire Harmer <claire.f.harmer@gmail.com>
2018-07-14 00:12:59 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,11 @@
 class Dog
-
-  def initialize(name)
+def initialize(name)
     @name = name
   end
 def name=(name)
   @name
   end
-
-  def initialize(breed = "Mutt")
+def initialize(breed = "Mutt")
     @breed = breed
   end
 def breed=(breed)
